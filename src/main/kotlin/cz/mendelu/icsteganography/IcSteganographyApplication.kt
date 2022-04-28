7f27f452e6da4b2c2dbf6b13b66f6fe9ede32072
Ivan Crnjak <icrnjak@protonmail.com>
2022-04-28 16:42:31 +0200

    Initial commit

---
+++ src/main/kotlin/cz/mendelu/icsteganography/IcSteganographyApplication.kt
@@ -0,0 +1,11 @@
+package cz.mendelu.icsteganography
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IcSteganographyApplication
+
+fun main(args: Array<String>) {
+    runApplication<IcSteganographyApplication>(*args)
+}
---
+++ src/test/kotlin/cz/mendelu/icsteganography/IcSteganographyApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.mendelu.icsteganography
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IcSteganographyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
