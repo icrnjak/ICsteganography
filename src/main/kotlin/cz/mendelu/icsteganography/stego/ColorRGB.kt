84c458c88ca94d52dbd540a4caca0bbd7552f16f
Ivan Crnjak <icrnjak@protonmail.com>
2022-05-04 16:31:45 +0200

    better format colors in gif comparison

--- src/main/kotlin/cz/mendelu/icsteganography/stego/ColorRGB.kt
+++ src/main/kotlin/cz/mendelu/icsteganography/stego/ColorRGB.kt
@@ -4,4 +4,12 @@ data class ColorRGB(
     var red: UByte,
     var green: UByte,
     var blue: UByte
-)
+) {
+    fun toColorString(): String {
+        return "#%02x%02x%02x".format(red.toByte(), green.toByte(), blue.toByte())
+    }
+
+    fun toHexString(): String {
+        return "%02x %02x %02x".format(red.toByte(), green.toByte(), blue.toByte()).uppercase()
+    }
+}
